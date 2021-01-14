ca19b1e85a5b461b5d077b91e5a1737cf40c8726
koki-pro <koki.banana03@gmail.com>
2021-01-14 21:09:07 +0900

    ラーメンの一覧表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'ramen_shops/index'
-  get 'ramen_shops/show'
   devise_for :users
   root 'homes#top'
+  resources :ramen_shops, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
