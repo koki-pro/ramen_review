ea974adb9c0a364aa59e48e66cbb53beefad60ab
koki-pro <koki.banana03@gmail.com>
2021-01-14 19:02:19 +0900

    ramen_shops コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'ramen_shops/index'
+  get 'ramen_shops/show'
   devise_for :users
   root 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
